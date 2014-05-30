9dd6bd33ea18b1b29a3d5523f8ae946d1ff54503
Dustin Getz <dustin.getz@gmail.com>
2014-05-30 08:47:15 -0400

    ReactCursor wip - need to track pendingState

--- form-master-detail/webapp/js/PageFinished.js
+++ form-master-detail/webapp/js/PageFinished.js
@@ -1,6 +1,6 @@
 /** @jsx React.DOM */
 define([
-    'underscore', 'react', 'jquery', 'wingspan-forms', 'Cursor', 'wingspan-contrib', 'react-json-editor',
+    'underscore', 'react', 'jquery', 'wingspan-forms', 'ReactCursor', 'wingspan-contrib', 'react-json-editor',
     'CustomBatchingStrategy',
     'util',
     'text!textassets/types/Contact.json',
@@ -12,16 +12,19 @@ define([
 
     var App = React.createClass({
         getInitialState: function () {
-            return { counts: _.range(1000).map(function () { return ''+0; }) };
+            return { counts: _.range(100).map(function () { return 0; }) };
         },
 
         componentWillMount: function () {
-            this.cursor = Cursor.build(this.state, this.setState.bind(this), util.deepClone);
+            window.App = this;
         },
 
         render: function () {
-            var fun = this.state.counts.map(function (count, index) {
-                return (<Clicker key={index} cursor={this.cursor.refine('counts', index)} />);
+            var cursor = Cursor.build(this.state, this.setState.bind(this), util.deepClone);
+            var counts = cursor.refine('counts');
+
+            var fun = counts.value.map(function (count, index) {
+                return (<Clicker key={index} cursor={counts.refine(index)} />);
             }.bind(this));
             return (
                 <div>
@@ -37,31 +40,38 @@ define([
         render: function () {
             return (
                 <div>
-                    <input type="text" value={this.props.cursor.value()} onChange={this.onInputChange} />
-                    <span>{this.props.cursor.value()}</span>
+                    <input type="text" value={this.props.cursor.value} onChange={this.onInputChange} />
+                    <span>{this.props.cursor.value}</span>
                     <button onClick={this.inc2}>+2</button>
                     <button onClick={this.inc10}>+10</button>
                 </div>
             );
         },
 
+        componentWillMount: function () {
+            this.cursor = this.props.cursor;
+        },
+
+        componentWillUpdate: function (nextProps) {
+            this.cursor = nextProps.cursor;
+        },
+
         onInputChange: function (e) {
-            this.props.cursor.onChange(e.target.value);
+            this.cursor = this.cursor.onChange(parseInt(e.target.value, 10));
         },
 
         inc2: function () {
-            var countCursor = this.props.cursor;
-            countCursor.onChange(countCursor.value() + 1);
-            countCursor.onChange(countCursor.value() + 1);
+            this.cursor = this.cursor.onChange(this.cursor.value + 1);
+            this.cursor = this.cursor.onChange(this.cursor.value + 1);
         },
 
         inc10: function () {
-            var countCursor = this.props.cursor;
-            countCursor.onChange(countCursor.value() + 10);
+            this.cursor = this.cursor.onChange(this.cursor.value + 10);
         },
 
         shouldComponentUpdate: function (nextProps) {
-            return this.props.cursor.value() != nextProps.cursor.value();
+            return true;
+            //this.props.cursor.value !== nextProps.cursor.value;
         }
     });
 
---
+++ form-master-detail/webapp/js/ReactCursor.js
@@ -0,0 +1,68 @@
+define([], function () {
+  'use strict';
+
+  // this constructor is private
+  function Cursor(state, path, commit, clone) {
+    state = clone(state); // defensive clone right away so we can't close over a stale state reference
+    this.value = getRefAtPath(state, path);
+
+    this.onChange = function (nextValue) {
+      var nextState;
+      nextValue = clone(nextValue); // because the call site might retain the reference and mutate
+
+      if (path.length > 0) {
+        nextState = state;
+        var scoped = getRefAtPath(nextState, initial(path));
+        scoped[last(path)] = nextValue;
+      }
+      else if (path.length === 0) {
+        nextState = nextValue;
+      }
+      commit(nextState);
+      return new Cursor(nextState, path, commit, clone);
+    };
+
+    this.refine = function (/* one or more paths through the tree */) {
+      var nextPath = [].concat(path, flatten(arguments));
+      return new Cursor(state, nextPath, commit, clone);
+    };
+  }
+
+
+  /**
+   * Example usages:
+   * Cursor.build(this.state, this.setState.bind(this), _.cloneDeep);
+   * Cursor.build(this.notState, function (nextState) { merge(this.notReactState, nextState); }.bind(this), _.identity);
+   */
+  Cursor.build = function (state, commit, clone) {
+    return new Cursor(state, [], commit, clone);
+  };
+
+
+  function getRefAtPath(tree, paths) {
+    return reduce(paths, deref, tree);
+  }
+
+  function deref(obj, key) {
+    return obj[key];
+  }
+
+  function initial(array) {
+    return array.slice(0, array.length-1);
+  }
+
+  function last(array) {
+    return array[array.length-1];
+  }
+
+  function reduce(array, f, mzero) {
+    return array.reduce(f, mzero);
+  }
+
+  function flatten(listOfLists) {
+    return [].concat.apply([], listOfLists);
+  }
+
+
+  return Cursor;
+});
