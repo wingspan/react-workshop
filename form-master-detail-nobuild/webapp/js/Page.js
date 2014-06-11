7d41d0ceb682c356c89c6cf1b55b6fba29d9114e
Daniel Miladinov <dmiladinov@wingspan.com>
2014-06-11 13:10:44 -0400

    Save QCon Talk Ending State
    Slides available at:
    
    http://www.dustingetz.com/2014/03/16/react-into-workshop-notes.html

--- form-master-detail-nobuild/webapp/js/Page.js
+++ form-master-detail-nobuild/webapp/js/Page.js
@@ -1,20 +1,108 @@
 /** @jsx React.DOM */
-define(['underscore', 'react'],
-function (_, React) {
+define(['underscore', 'react', 'wingspan-forms', 'text!./fieldInfos.json', 'Cursor'],
+function (_, React, Forms, fieldInfosJson, Cursor) {
     'use strict';
 
+    var KendoText = Forms.KendoText;
+    var KendoComboBox = Forms.KendoComboBox;
+    var FormField = Forms.FormField;
+    var AutoControl = Forms.AutoControl;
+    var fieldInfos = JSON.parse(fieldInfosJson);
 
-
-    var Hello = React.createClass({
-        render: function() {
+    var PrettyJson = React.createClass({
+        render: function () {
             return (
-                <div>Hello {this.props.name}</div>
+                <pre>{JSON.stringify(this.props.value, undefined, 2)}</pre>
+            );
+        }
+    });
+
+    var AutoField = React.createClass({
+        render: function () {
+            return (
+                <FormField fieldInfo={this.props.fieldInfo} isValid={this.props.isValid}>
+                    <AutoControl
+                    value={this.props.value}
+                    onChange={this.props.onChange}
+                    fieldInfo={this.props.fieldInfo} />
+                </FormField>
+            );
+        }
+    });
+
+    var Form = React.createClass({
+        render: function () {
+            var controls = _.map(fieldInfos, function (fieldInfo) {
+                var fieldCursor = this.props.cursor.refine(fieldInfo.name);
+                return (
+                    <AutoField
+                    fieldInfo={fieldInfo}
+                    value={fieldCursor.value}
+                    onChange={fieldCursor.onChange} />
                 );
+            }.bind(this));
+
+            return (
+                <div>
+                    {controls}
+                </div>
+            );
+        }
+    });
+
+    var App = React.createClass({
+        componentWillMount: function () {
+            window.app = this;
+        },
+        getInitialState: function () {
+            return {
+                form: { firstName: '', lastName: '', gender: '', age: null, birthday: null },
+                list: [
+                    { firstName: 'Alice', lastName: '', gender: '', age: null, birthday: null },
+                    { firstName: 'Bob', lastName: '', gender: '', age: null, birthday: null },
+                    { firstName: 'Charlie', lastName: '', gender: '', age: null, birthday: null }
+                ]
+            }
+        },
+        componentDidUpdate: function(prevProps, prevState) {
+            // compare prevState to this.state?
+            // compare prevProps to this.props?
+        },
+        render: function () {
+            var cursor = Cursor.build(this.state, this.setState.bind(this));
+            var list = _.map(this.state.list, function (record) {
+                return (
+                    <li>
+                        <button onClick={_.partial(cursor.refine('form').onChange, record)}>
+                        {record.firstName}
+                        </button>
+                    </li>
+                    );
+            }.bind(this));
+            return (
+                <div className="App">
+                    <ol>{list}</ol>
+                    <Form cursor={cursor.refine('form')} />
+                    <PrettyJson value={this.state} />
+                </div>
+            );
+        },
+        onChange: function (fieldName, fieldValue) {
+            var nextState = {};
+            nextState[fieldName] = fieldValue;
+            this.setState(nextState);
+        },
+        isFieldValid: function (fieldName) {
+            return !!this.state[fieldName] ? [true, ''] : [false, 'You must fill this in'];
         }
     });
 
     function entrypoint() {
-        React.renderComponent(<Hello name="World" />, document.getElementById('root'));
+        window.App = App;
+        window.React = React;
+        window.Cursor = Cursor;
+        React.renderComponent(<App />, document.getElementById('root'));
+        Forms.ControlCommon.attachFormTooltips($('body'));
     }
 
     return { entrypoint: entrypoint };
